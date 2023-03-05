e66b4472dfe2c7fa867da905a1c0d08eeaf25b19
Tuckett45 <tucausti@icloud.com>
2023-03-04 22:10:55 -0600

    header styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/home-page/header/header.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { AuthService } from 'src/shared/services/auth.service';
 
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from 'src/shared/services/auth.service';
 import { DistrictsModel } from 'src/shared/models/districts.model';
 import { RegionsModel } from 'src/shared/models/regions.model';
 import { StoresModel } from 'src/shared/models/stores.model';
-import { HeaderComponent } from '../header/header.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'home-page',
